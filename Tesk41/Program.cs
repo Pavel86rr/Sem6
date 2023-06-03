5f24b406527a1eed0767783501a6039b101637d4
«Pavel <kskles@yandex.ru>
2023-06-03 10:54:54 +0700

    Задача41

---
+++ Tesk41/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+ Console.WriteLine("Введите элементы через пробел ");
+
+ int[] inputArray = Array.ConvertAll(Console.ReadLine()!.Split(),int.Parse);
+
+ int count =0;
+ int size = inputArray.Length;
+ for(int i=0; i < size; i++)
+   {
+     if(inputArray [i]>0)
+    {
+        count=count + 1;
+    }
+   }
+ Console.WriteLine(count);
